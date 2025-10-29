a645c872d54c9e13a960feed8629ef904b48e2ef
Eds Ibalio <johnedisond.ibalio@gmail.com>
2025-10-30 00:10:23 +0800

    disable choices at winningScore and add win and lose color

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ let computerScore = 0;
 let winningScore = 3;
 let isGameOver = false;
 
+const disableChoices = () => {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissor.disabled = true;
+    setTimeout(() =>{
+        rock.classList.remove("selection");
+        paper.classList.remove("selection");
+        scissor.classList.remove("selection");
+    },1500);
+}
+
 for (let playerClicked of playerSelect) {
     playerClicked.addEventListener("click", () => {
         const playerChoice = playerClicked.id;
@@ -48,9 +59,19 @@ function playRound(playerChoice) {
     }
     console.log(result);
 
+    displayGameResult.classList.remove("winBgColor", "loseBgColor");
+    displayGameResult.classList.remove("winTextColor", "loseTextColor");
+
     displayPlayerChoice.textContent = `YOU: ${playerChoice.toUpperCase()}`;
     displayComputerChoice.textContent = `COMPUTER: ${computerChoice.toUpperCase()}`;
     displayGameResult.textContent = result;
+
+    if (result === "YOU WIN!") {
+        displayGameResult.classList.add("winBgColor");
+
+    }else if (result === "YOU LOSE!") {
+        displayGameResult.classList.add("loseBgColor");
+    }
 
 
     // -display Score-
@@ -71,10 +92,18 @@ function playRound(playerChoice) {
     }
 
     if (playerScore === winningScore){
+        displayPlayerScore.classList.add("winTextColor");
+        displayComputerScore.classList.add("loseTextColor");
+        displayWinnerResult.classList.add("winTextColor");
         displayWinnerResult.textContent = "GAME OVER! YOU WIN THIS ROUND!";
+        disableChoices();
 
     }else if (computerScore === winningScore) {
+        displayPlayerScore.classList.add("loseTextColor");
+        displayComputerScore.classList.add("winTextColor");
+        displayWinnerResult.classList.add("loseTextColor");
         displayWinnerResult.textContent = "GAME OVER! COMPUTER WINS THIS ROUND!";
+        disableChoices();
     }
 }
 
