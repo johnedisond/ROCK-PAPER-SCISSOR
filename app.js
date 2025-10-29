de276ad8b827d931d5c8d2c2e0ef7fde5cfb5df9
Eds Ibalio <johnedisond.ibalio@gmail.com>
2025-10-29 21:52:22 +0800

    connect the random computerChoice to playerChoice

---
+++ app.js
@@ -0,0 +1,22 @@
+const playerSelect = document.querySelectorAll(".selection");
+const displayPlayerScore = document.querySelector(".displayPlayerScore");
+const displayComputerScore = document.querySelector(".displayComputerScore");
+const displayPlayerChoice = document.querySelector(".displayPlayerChoice");
+const displayComputerChoice = document.querySelector(".displayComputerChoice");
+const displayGameResult = document.querySelector(".displayGameResult");
+
+
+const choices = ["rock", "paper", "scissor"];
+
+for (let playerClicked of playerSelect) {
+    playerClicked.addEventListener("click", () => {
+        const playerChoice = playerClicked.id;
+        console.log(playerChoice);
+        playRound(playerChoice);
+    })
+}
+
+function playRound(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(computerChoice);
+}
